93f2a418490c83dcbcc5e03a233667cd8bd872f4
Meghna Magesh <mages8078@mydusd.org>
2025-12-05 14:13:11 -0800

    12/5/25

--- ImageApp.java
+++ ImageApp.java
@@ -26,7 +26,7 @@ public class ImageApp
     for (int row = 0; row < recoloredPixels.length; row++) {
       for (int col = 0; col < recoloredPixels[0].length; col++) {
         Pixel p = origPixels[row][col];
-        recoloredPixels[row][col].setColor(new Color(p.getBlue(), p.getGreen(), p.getRed()));
+        //recoloredPixels[row][col].setColor(new Color(p.getBlue(), p.getGreen(), p.getRed()));
         //recoloredPixels[row][col].setColor(new Color(p.getGreen(), p.getRed(), p.getBlue()));
         //recoloredPixels[row][col].setColor(new Color(p.getRed(), p.getBlue(), p.getGreen()));
       }
@@ -85,6 +85,26 @@ public class ImageApp
     // Final Image: Add a small image to a larger one
 
     /* to be implemented */
+    Picture largeImg = new Picture ("lib/bridge.jpg");
+    Pixel[][] largePixels = largeImg.getPixels2D();
+    Picture smallImg = new Picture ("lib2/bird.png");
+    Pixel[][] smallPixels = smallImg.getPixels2D();
+    int startRow = (largePixels.length - smallPixels.length) / 2;
+    int startCol = (largePixels[0].length - smallPixels[0].length) / 2;
+    if (startRow < 0) startRow = 0;
+    if (startCol < 0) startCol = 0;
+
+    for (int r = 0; r < smallPixels.length; r++) {
+      for (int c = 0; c < smallPixels[0].length; c++) {
+        int lr = startRow + r;
+        int lc = startCol + c;
+        if (lr >= 0 && lr < largePixels.length && lc >= 0 && lc < largePixels[0].length) {
+          largePixels[lr][lc].setColor(smallPixels[r][c].getColor());
+        }
+      }
+    }
+
+    largeImg.explore();
 
     // for testing  2D algorithms
     int[][] test1 = { { 1, 2, 3, 4 },
---
+++ UserMenu.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class UserMenu {
+    Scanner s = new Scanner(System.in);
+    public static void displayMenu() {
+        System.out.println("Welcome to the Image App!");
+        System.out.println("Please choose an option:");
+        System.out.println("1. Recolor Image");
+        System.out.println("2. Create Photographic Negative");
+        System.out.println("3. Create Grayscale Image");
+        System.out.println("4. Exit");
+        
+    }
+}
